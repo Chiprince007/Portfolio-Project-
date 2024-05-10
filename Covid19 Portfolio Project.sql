8b5750de93e1485f15f2bc5fe0928a05ff6988d4
Chiprince007 <158818488+Chiprince007@users.noreply.github.com>
2024-05-10 11:06:58 +0100

    Add files via upload

---
+++ Covid19 Portfolio Project.sql
@@ -0,0 +1,114 @@
+
+select Location, date,new_cases,total_deaths,population
+from [Portfolio Project]..[covid-deaths]
+order by 1,2
+
+--TOTAL CASES VS TOTAL DEATHS
+
+select Location, date,total_cases,total_deaths,(cast(total_deaths as float)/cast(total_cases as float))*100 FatalityRate
+from [Portfolio Project]..[covid-deaths]
+where location like '%geria'
+order by 1,2
+
+select Location, date,total_cases,population,(total_cases/population)*100 InfectionRate
+from [Portfolio Project]..[covid-deaths]
+--where location like '%geria'
+order by 1,2
+
+
+--countries with higjhest infection rate
+select Location,population,max(total_cases)HighestInfectionCount,Max((total_cases/population))*100 InfectionRate
+from [Portfolio Project]..[covid-deaths]
+--where location like '%geria'
+group by location,population
+order by InfectionRate desc
+
+
+--- Highest Death count per population
+select location,max(cast(total_deaths as int)) TotalDeathCount
+from [Portfolio Project]..[covid-deaths]
+where continent is not null
+group by location
+order by TotalDeathCount desc
+
+---- CONTINENTS
+select location,max(cast(total_deaths as int)) TotalDeathCount
+from [Portfolio Project]..[covid-deaths]
+where continent is null
+group by location
+order by TotalDeathCount desc
+
+----- GLOBAL NUMBERS 
+select sum(cast(new_cases as int)) TotalCases,sum(cast(new_deaths as int)) TotalDeath,(sum(cast(new_deaths as float))/sum(cast(new_cases as float)))*100 DeathPercentage --total_deaths,(cast(total_deaths as int)/total_cases)*100 DeathPercentage
+from [Portfolio Project]..[covid-deaths]
+where continent is not null
+--group by date
+order by 1,2
+
+
+select cvd.continent,cvd.location,cvd.date,cvd.population,cvv.new_vaccinations
+,sum(cast (cvv.new_vaccinations as float)) over (partition by cvd.location order by cvd.location,cvd.date) RollingVaccination
+from [Portfolio Project]..[covid-deaths] cvd
+join [Portfolio Project]..[covid vaccination] cvv
+	on cvd.location=cvv.location
+	and cvd.date=cvv.date
+where cvd.continent is not null
+order by 2,3
+
+
+
+WITH POPVAC (continent,location,date,population,new_vaccinations,RollingVaccination)
+as
+(
+select cvd.continent,cvd.location,cvd.date,cvd.population,cvv.new_vaccinations
+,sum(cast (cvv.new_vaccinations as float)) over (partition by cvd.location order by cvd.location,cvd.date) RollingVaccination
+from [Portfolio Project]..[covid-deaths] cvd
+join [Portfolio Project]..[covid vaccination] cvv
+	on cvd.location=cvv.location
+	and cvd.date=cvv.date
+where cvd.continent is not null
+--order by 2,3
+)
+select *, (RollingVaccination/population)*100 PercentPopulationVaccinated
+from POPVAC
+
+---- TEMPTABLE
+
+CREATE table #percentpopulationvaccinated
+( 
+continent nvarchar(255),
+location nvarchar(255),
+date datetime,
+population numeric,
+new_vaccinations numeric,
+RollingVaccination numeric
+)
+
+insert into #percentpopulationvaccinated
+select cvd.continent,cvd.location,cvd.date,cvd.population,cvv.new_vaccinations
+,sum(cast (cvv.new_vaccinations as float)) over (partition by cvd.location order by cvd.location,cvd.date) RollingVaccination
+from [Portfolio Project]..[covid-deaths] cvd 
+join [Portfolio Project]..[covid vaccination] cvv
+	on cvd.location=cvv.location
+	and cvd.date=cvv.date
+where cvd.continent is not null
+--order by 2,3
+
+select *, (RollingVaccination/population)*100 PercentPopulationVaccinated
+from #percentpopulationvaccinated
+
+
+--- creating view
+
+create view populationvaccinated as
+select cvd.continent,cvd.location,cvd.date,cvd.population,cvv.new_vaccinations
+,sum(cast (cvv.new_vaccinations as float)) over (partition by cvd.location order by cvd.location,cvd.date) RollingVaccination
+from [Portfolio Project]..[covid-deaths] cvd 
+join [Portfolio Project]..[covid vaccination] cvv
+	on cvd.location=cvv.location
+	and cvd.date=cvv.date
+where cvd.continent is not null
+--order by 2,3
+
+select *
+from PercentPopulationVaccinate
